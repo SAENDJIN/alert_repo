0bfc0cd326a8a3cb984a293638da04774d20acef
SAENDJIN <andrey.saenko31@gmail.com>
2022-08-15 14:49:13 +0300

    123

--- alert_test/alert_test.py
+++ alert_test/alert_test.py
@@ -10,11 +10,12 @@ def run(playwright: Playwright) -> None:
     page.locator("button:has-text(\"S-Z\")").click()
     page.locator("button:has-text(\"J-R\")").click()
 
+    # result = \
     expect(page.locator(
-        "//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]")) \
-        .to_contain_text("slo1tvibe.com")
+            "//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]")) \
+            .to_contain_text("slotvibe.com")
 
-    # assert
+    # assert result
     context.close()
     browser.close()
 
