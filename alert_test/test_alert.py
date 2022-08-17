a3e97ce7b7c1a8c01e3c7c1baebb7ceae61be8c9
SAENDJIN <andrey.saenko31@gmail.com>
2022-08-17 20:20:54 +0300

    Рабочий код для женкинса, тест не работает (всегда успех)

--- alert_test/test_alert.py
+++ alert_test/test_alert.py
@@ -1,7 +1,7 @@
 
 
 def test_alert_reporting(australia_check_oxi):
-    australia_check_oxi.casino_checkё
+    australia_check_oxi.casino_check
     assert australia_check_oxi.check_result
     australia_check_oxi.close
 # def test_alert_repoting(au)
