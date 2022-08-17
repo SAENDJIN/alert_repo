5c3ec6c5e7d9a5ba389da4a4bd816ca15d0db911
SAENDJIN <andrey.saenko31@gmail.com>
2022-08-17 20:18:55 +0300

    LOCKED CODE

--- alert_test/alert_test.py
+++ alert_test/alert_test.py
@@ -1,33 +1,33 @@
-from playwright.sync_api import Playwright, sync_playwright
-
-
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://www.acma.gov.au/blocked-gambling-websites#blocked-illegal-gambling-websites", timeout=0)
-
-    page.locator("button:has-text(\"S-Z\")").click()
-    page.locator("button:has-text(\"J-R\")").click()
-
-    # result = page.inner_text("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]"
-    #                          "/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/"
-    #                          "div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]") == 'slotvibe.com'
-    # assert result == "slotvibe.com"
-    # assert page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div["
-    #                     "2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/d"
-    #                     "iv[1]/div[2]/div[1]/div[1]/p[1]").input_value("slotvibe.com")
-    text = page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div["
-                        "2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/d"
-                        "iv[1]/div[2]/div[1]/div[1]/p[1]").text_content()
-    assert "slotsberlin5.com" in text
-    # expect(page.locator(
-    #     "//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]")) \
-    #     .to_contain_text("slotvibe.com")
-
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
+# from playwright.sync_api import Playwright, sync_playwright
+#
+#
+# def run(playwright: Playwright) -> None:
+#     browser = playwright.chromium.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+#     page.goto("https://www.acma.gov.au/blocked-gambling-websites#blocked-illegal-gambling-websites", timeout=0)
+#
+#     page.locator("button:has-text(\"S-Z\")").click()
+#     page.locator("button:has-text(\"J-R\")").click()
+#
+#     # result = page.inner_text("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]"
+#     #                          "/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/"
+#     #                          "div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]") == 'slotvibe.com'
+#     # assert result == "slotvibe.com"
+#     # assert page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div["
+#     #                     "2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/d"
+#     #                     "iv[1]/div[2]/div[1]/div[1]/p[1]").input_value("slotvibe.com")
+#     text = page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div["
+#                         "2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/d"
+#                         "iv[1]/div[2]/div[1]/div[1]/p[1]").text_content()
+#     assert "slotsberlin5.com" in text
+#     # expect(page.locator(
+#     #     "//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div[2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/div[1]/div[2]/div[1]/div[1]/p[1]")) \
+#     #     .to_contain_text("slotvibe.com")
+#
+#     context.close()
+#     browser.close()
+#
+#
+# with sync_playwright() as playwright:
+#     run(playwright)
