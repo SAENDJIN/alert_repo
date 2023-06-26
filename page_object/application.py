a0aec5f2bb26a0b252bb885d9dea8d48223101c0
SAENDJIN <andrey.saenko31@gmail.com>
2023-06-26 11:12:49 +0300

    fix

--- page_object/application.py
+++ page_object/application.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Playwright
 
 class AlertSz:
     def __init__(self, playwright: Playwright):
-        self.browser = playwright.chromium.launch(headless=False)
+        self.browser = playwright.chromium.launch(headless=False, slow_mo=500)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
         self.page.goto("https://www.acma.gov.au/blocked-gambling-websites#blocked-illegal-gambling-websites", timeout=0)
@@ -19,7 +19,7 @@ class AlertSz:
         textsz = self.page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div["
                                    "2]/div[2]/article[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[5]/d"
                                    "iv[1]/div[2]/div[1]/div[1]/p[1]").text_content()
-        return "zoome1.casino" and "zoome.casino" and "thezoome.com" not in textsz
+        return ["zoome1.casino", "zoome.casino", "thezoome.com"] not in textsz
 
     def close(self):
         self.context.close()
@@ -40,9 +40,9 @@ class AlertJr:
     # Oxi test
     def check_result_jr(self):
         textjr = self.page.locator("//html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/main[1]/div[2]/div[2]/article[1]/div"
-                                   "[1]/div[2]/div[6]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/p[1]")\
+                                   "[1]/div[2]/div[6]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/p[1]") \
             .text_content()
-        return "oxi.casino" and "oxi1.casino" not in textjr
+        return ["oxi.casino", "oxi1.casino"] not in textjr
 
     def close(self):
         self.context.close()
